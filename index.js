da28c5c436f9d10916f87feec897e5456db1c626
CheyenneF <106608168+CheyenneF@users.noreply.github.com>
2022-06-06 14:04:48 -0400

    firstTry

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
+const mongoose = require('mongoose');
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,3 +16,13 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
+mongoose.connect('mongodb://localhost:27017/productsdb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('conncection successful');
+    }).catch((error) => {
+        console.log("something went wrong");
+    })
