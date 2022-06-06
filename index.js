7fe7896cd4c34773aae9be23be563ed07d8361a2
CheyenneF <106608168+CheyenneF@users.noreply.github.com>
2022-06-06 14:25:01 -0400

    take 2

--- index.js
+++ index.js
@@ -21,8 +21,20 @@ mongoose.connect('mongodb://localhost:27017/productsdb',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
+    }).then(() =>{
+        console.log('success');
+    }).catch((error) => {
+        console.log('error', error);
+    })
+
+/*
+mongoose.connect('mongodb://localhost:27017/productsdb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     }).then(() => {
         console.log('conncection successful');
     }).catch((error) => {
         console.log("something went wrong");
     })
+*/
