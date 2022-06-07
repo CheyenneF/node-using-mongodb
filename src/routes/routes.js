a89a05edbc06f2c188f0b60fedb09eab9c21337d
CheyenneF <106608168+CheyenneF@users.noreply.github.com>
2022-06-07 11:48:11 -0400

    4-4

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
 import { addnewProduct } from '../controllers/controllers';
 
 import { getProducts } from '../controllers/controllers';
+import { getProductWithID } from '../controllers/controllers';
+
 
 const routes = (app) => {
 
     app.route('/products');
     app.route.get(getProducts);
     app.routes.post(addnewProduct);
+
+    app.route('/products/:ProductID');
+    app.route.get(getProductWithID);
     
     
 } ;
