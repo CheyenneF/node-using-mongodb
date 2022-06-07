c23a05f55af6c793f67421b884335c418364bee1
CheyenneF <106608168+CheyenneF@users.noreply.github.com>
2022-06-07 08:48:22 -0400

    module1

--- src/models/models.js
+++ src/models/models.js
@@ -2,14 +2,33 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const modelSchema = new Schema();
+export const ProductSchema = new Schema({
+  name: {type: String, required: 'Product name'},
+  description: {type: String, required: 'Description'},
+  category: {type: String},
+  price: {type: Number},
+  created_date: {type: Date,default: Date.now}
+});
 
 
-modelSchema.add({
-  name:  String, 
-  description: String,
+/*
+export const modelSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: String,
+  price: Number,
+  created_date: { type: Date, default: Date.now }
+
+});
+
+
+emodelSchema.add({
+  name:  { type: String, required: true }, 
+  description: { type: String, required: true },
   category:   String,
   price: Number,
   created_date: { type: Date, default: Date.now }
   
 });
+
+*/
